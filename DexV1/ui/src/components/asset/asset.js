8059b7cd2925596860ffe0615b6349e134df4866
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-01-27 15:36:49 +0545

    Added flipping functionality

--- DexV1/ui/src/components/asset/asset.js
+++ DexV1/ui/src/components/asset/asset.js
@@ -6,12 +6,24 @@ import { ethers } from "ethers";
 import ABI from "../../ERC20ABI.json";
 import { Link } from "react-router-dom";
 export default function Asset(props){
+
+    const ethImgPath = "https://res.cloudinary.com/sushi-cdn/image/fetch/f_auto,fl_sanitize,q_auto,w_48/https://raw.githubusercontent.com/sushiswap/list/master/logos/native-currency-logos/ethereum.svg";
+    //ticker symbol 
+    const [tickerSymbol1,setTickerSymbol1] = useState("ETH");
+    const [tickerSymbol2,setTickerSymbol2] = useState("ARP");
+
+    //Ticker logo path
+    const [tickerImg1, setTickerImg1] = useState(ethImgPath);
+    const [tickerImg2,setTickerImg2] = useState(AraswapLogo);
+
+
+    
+    //Input state
     const [input1, setinput1] = useState("");
     const [width,setWidth] = useState(35);
     const [inputUSDisplay,setInputUSDisplay] = useState("none");
     const inputRef = useRef(null);
     const inputUSD = useRef(null);
-
 
     const [input2, setinput2] = useState("");
     const [width2,setWidth2] = useState(35);
@@ -118,8 +130,8 @@ export default function Asset(props){
     }
 
     //Get balance of the user from cookie
-    const [ethbalance,setBalance] = useState(0);
-    const [ARPBalance,setARPBalance] = useState(0);
+    const [balance1,setBalance1] = useState(0);
+    const [balance2,setBalance2] = useState(0);
     const [cookies,setCookie] = useCookies(['WalletAddress']);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -127,7 +139,7 @@ export default function Asset(props){
     provider.getBalance(cookies.WalletAddress).then((value)=>{
         let formattedBalance = ethers.utils.formatUnits(value._hex, "ether").slice(0,6);
         // console.log("Balanced fetched: ",formattedBalance);
-        setBalance(formattedBalance);
+        setBalance1(formattedBalance);
     }).catch((error)=>{
       console.log("Get Balance error: ", error);
     });
@@ -137,10 +149,32 @@ export default function Asset(props){
     contract.balanceOf(cookies.WalletAddress).then((value)=>{
         let formattedBalance = ethers.utils.formatUnits(value._hex, "ether");
         // console.log("ARP balance fetched: ",formattedBalance);
-        setARPBalance(formattedBalance);
+        setBalance2(formattedBalance);
     }).catch((error)=>{
         console.log("Get ARP balance error: ",error);
     });
+
+
+    //Asset flipping logic for Swap
+    function flipAsset(){
+
+
+        //Swapping ticker symbol
+        const tempTicker1 = tickerSymbol1;
+        setTickerSymbol1(tickerSymbol2);
+        setTickerSymbol2(tempTicker1);
+
+        //Swapping logo
+        const tempLogo1 = tickerImg1;
+        setTickerImg1(tickerImg2);
+        setTickerImg2(tempLogo1);
+
+        //Swapping balance
+        const tempBalance1 = balance1;
+        setBalance1(balance2);
+        setBalance2(tempBalance1);
+
+    }
 
 
     return(
@@ -149,9 +183,9 @@ export default function Asset(props){
             <div className="asset">
                 <div className="ticker">
                     <div className="ethImg">
-                        <img src="https://res.cloudinary.com/sushi-cdn/image/fetch/f_auto,fl_sanitize,q_auto,w_48/https://raw.githubusercontent.com/sushiswap/list/master/logos/native-currency-logos/ethereum.svg"></img>
+                        <img src={tickerImg1}></img>
                     </div>
-                    <span>ETH</span>
+                    <span>{tickerSymbol1}</span>
                 </div>
                 <br/>
                 <div className="input-amount">
@@ -159,17 +193,24 @@ export default function Asset(props){
                         <input placeholder="0" id="firstinput" value={input1} onChange={handleChange} ref={inputRef} style={{width: `${width}px`}}></input>
                         <span ref={inputUSD} style={{display:`${inputUSDisplay}` }}>~${input1*1000}</span>
                     </div>
-                    <span className="balance">Balance: {ethbalance}</span>
+                    <span className="balance">Balance: {balance1}</span>
 
                 </div>
             </div>
-            <br/>
+            <div className="motionContainer" onClick={flipAsset}>
+                <div className="flipIconContainer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="2" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>            
+
+                </div>
+
+            </div>
+            {/* <br/> */}
             <div className="asset">
                 <div className="ticker">
                     <div className="ethImg">
-                        <img src={AraswapLogo}></img>
+                        <img src={tickerImg2}></img>
                     </div>
-                    <span>ARP</span>
+                    <span>{tickerSymbol2}</span>
                 </div>
                 <br/>
                 <div className="input-amount">
@@ -177,7 +218,7 @@ export default function Asset(props){
                         <input placeholder="0" id="secondinput" value={input2} onChange={handleChange} ref={inputRef2} style={{width: `${width2}px`}}></input>
                         <span ref={inputUSD2} style={{display:`${inputUSDisplay2}` }}>~${input2*1000}</span>
                     </div>
-                    <span className="balance">Balance: {ARPBalance}</span>
+                    <span className="balance">Balance: {balance2}</span>
 
                 </div>
             </div>
@@ -185,7 +226,6 @@ export default function Asset(props){
 
                 {cookies.WalletAddress == undefined ? <Link to='/connect'><button className="addLiquidbtn"> Connect Wallet</button></Link>:<button className="addLiquidbtn">{props.type == "Swap"? "Swap":"Add Liquidity"}</button> }
             </div>
-
 
         </div>
     )
