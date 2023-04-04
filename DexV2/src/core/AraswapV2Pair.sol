8c2702dc655f408aa262f36dc24b6b567f93887c
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-04-04 21:09:17 +0545

    Added burn

--- DexV2/src/core/AraswapV2Pair.sol
+++ DexV2/src/core/AraswapV2Pair.sol
@@ -23,13 +23,27 @@ contract AraswapV2Pair is ERC20,Math{
 
 
     // Events
-    event Mint(address indexed msg.sender,uint256 amount0,uint256 amount1);
+    event Mint(address indexed sender,uint256 amount0,uint256 amount1);
     event Sync(uint256 reserve0,uint256 reserve1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
 
     //Constructor for LP pair token
     constructor(address _token0, address _token1)ERC20("AraswapV2 Pair","ARPV2",18){
         token0 = _token0;
         token1 = _token1;
+    }
+
+    // Safe transfer
+        function _safeTransfer(
+        address token,
+        address to,
+        uint256 value
+    ) private {
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSignature("transfer(address,uint256)", to, value)
+        );
+        if (!success || (data.length != 0 && !abi.decode(data, (bool))))
+            revert TransferFailed();
     }
 
     //Get reserves
@@ -73,6 +87,37 @@ contract AraswapV2Pair is ERC20,Math{
         _update(balance0,balance1);
         emit Mint(msg.sender,amount0,amount1);
         
+    }
+
+    // Removing liquidity 
+    function burn() public{
+        // Get the latest reserve
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 liquidity = balanceOf[msg.sender];  //Amount of LP tokens held by the user
+        // Amount of token that user should get
+        uint256 amount0 =  (balance0 * liquidity)/totalSupply;
+        uint256 amount1 = (balance1 * liquidity)/totalSupply;
+
+        if(amount0 <= 0 || amount1 <= 0){
+            revert("Insufficient liquidity burn amount");
+        }
+
+        //Burning liquidity
+        _burn(msg.sender,liquidity);
+
+        //Transferring the token amounts
+        _safeTransfer(token0,msg.sender,amount0);
+        _safeTransfer(token1,msg.sender,amount1);
+
+        // Getting latest reserve and updating it
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+
+        _update(balance0,balance1);
+
+        emit Burn(msg.sender,amount0,amount1);
+
     }
 
 
