1fec1a5d0ffaa4233011cd1d4153f0cb6e67ed23
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-04-07 23:32:50 +0545

    Implemented swap function

--- DexV2/src/core/AraswapV2Pair.sol
+++ DexV2/src/core/AraswapV2Pair.sol
@@ -18,14 +18,22 @@ contract AraswapV2Pair is ERC20,Math{
     address public token1;
 
     //Reserves of token pairs
-    uint256 private reserve0;
-    uint256 private reserve1;
+    uint112 private reserve0;
+    uint112 private reserve1;
+
+    // Last swap timestamp
+    uint32 private blockTimestampLast;
+
+    // Price calculation
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
 
 
     // Events
     event Mint(address indexed sender,uint256 amount0,uint256 amount1);
-    event Sync(uint256 reserve0,uint256 reserve1);
+    event Sync(uint112 reserve0,uint112 reserve1);
     event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Swap(address indexed sender, uint256 amount0out, uint256 amount1out,address to);
 
     //Constructor for LP pair token
     constructor(address _token0, address _token1)ERC20("AraswapV2 Pair","ARPV2",18){
@@ -33,35 +41,44 @@ contract AraswapV2Pair is ERC20,Math{
         token1 = _token1;
     }
 
-    // Safe transfer
-        function _safeTransfer(
-        address token,
-        address to,
-        uint256 value
-    ) private {
-        (bool success, bytes memory data) = token.call(
-            abi.encodeWithSignature("transfer(address,uint256)", to, value)
-        );
-        if (!success || (data.length != 0 && !abi.decode(data, (bool))))
-            revert TransferFailed();
-    }
-
+    
     //Get reserves
-    function getReserves() public returns(uint256,uint256){
+    function getReserves() public returns(uint112,uint112){
         return(reserve0,reserve1);
     }
 
     // Update reserves
-    function _update(uint256 _balance0, uint256 _balance1){
-        reserve0 = _balance0;
-        reserve1 = _balance1;
+    function _update(uint256 _balance0, uint256 _balance1)private{
+        reserve0 = uint112(_balance0);
+        reserve1 = uint112(_balance1);
+        emit Sync(reserve0,reserve1);
+    }
+
+    // Swap update
+    function _update_(uint256 _balance0, uint256 _balance1, uint112 _reserve0, uint112 _reserve1)private{
+        
+        unchecked{
+            // Time elapsed after the last swap
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if(timeElapsed >0 && _reserve0 > 0 && reserve1 > 0){
+                price0CumulativeLast += uint256(UQ112x112.encode(_reserve1).uqdiv(_reserve0))*timeElapsed;
+                price1CumulativeLast += uint256(UQ112x112.encode(_reserve0).uqdiv(_reserve1))*timeElapsed;
+            
+            }
+        }
+
+        
+        reserve0 = uint112(_balance0);
+        reserve1 = uint112(_balance1);
+        blockTimestampLast = uint32(block.timestamp);
         emit Sync(reserve0,reserve1);
     }
 
     //Mint LP tokens
     function mint() public{
 
-        (uint256 _reserve0, uint256 _reserve1) = getReserves();
+        (uint112 _reserve0, uint112 _reserve1) = getReserves();
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
 
@@ -120,7 +137,55 @@ contract AraswapV2Pair is ERC20,Math{
 
     }
 
+    // Swap
+    function swap(amount0out, amount1out, to) public{
 
+        //Output amount checks
+        if(amount0out <=0 || amount1out <=0){
+            revert("Insufficient amounts");
+        }
+
+        // Reserves
+        (uint112 r0, uint112 r0) = getReserves();
+
+        //Check if the ouput amount is greater than reserves
+        if(amount0out > r0 || amount1out > r1){
+            revert("Amount is greater than the reserves");
+        }
+
+        //Estimated balance of the contract after swap
+        uint256 _balance0 = IERC20(token0).balanceOf(address(this)) - amount0out;
+        uint256 _balance1 = IERC20(token1).balanceOf(address(this)) - amount1out;
+
+        // Check to ensure that the product of reserves are equal or greater than k
+        if((r0 * r1) > (_balance0 * _balance1)){
+            revert("Invalid amounts");
+        }
+
+        // Update reserve (swap)
+        _update_(_balance0,_balance1);
+
+        // Transfer token
+        if(amount0out > 0){
+            _safeTransfer(token0,to,amount0out);
+        }
+        if(amount1 > 0 ){
+            _safeTransfer(token1,to,amount1out);
+        }
+
+        //Emitting event
+        emit Swap(msg.sender,amount0out,amount1out,to);
+
+    }
+
+
+    // Safe transfer
+    function _safeTransfer(address token, address to, uint256 amount) private{
+        (bool success, bytes memory data) =  token.call(abi.encodeWithSignature("transfer(address,uint256)",to,amount));
+        if(!success || (data.length != 0 && !abi.decode(data,(bool)))){
+            revert("Transfer fail");
+        }
+    }
 
 
 }
